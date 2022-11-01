4ff60fcb520e5d3138d29485dbaf5f9e6eaacb9f
opico1 <carlosopico2@gmail.com>
2022-10-31 22:01:29 -0600

    Complete

--- Ejemplo-btc/btc/app.js
+++
@@ -1,8 +0,0 @@
-let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcbusd@trade');
-const price = document.querySelector('#stock-price');
-
-ws.onmessage = (event) => {
-    let stockObject = JSON.parse(event.data);
-    price.innerHTML = stockObject.p;
-    //console.log(stockObject.p);
-}
